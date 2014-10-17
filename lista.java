64531c91bff2547b9fe14a679c1e30ececfa21e1
Rodolfo Castillo <rovidrio@comunidad.unam.mx>
2014-10-17 11:45:54 -0500

    Cambio menor en listas
    menor

--- lista.java
+++ lista.java
@@ -7,7 +7,23 @@ public class lista{
     	inicio = fin = null;
     }
 
-    public void InsertarAlInicio(String Info){
+    public void InsertarAlInicio(String info){
     	Nodo nuevo = new Nodo(info, inicio);
+    	inicio = nuevo;
+    	if (fin == null) 
+    		fin = inicio;
     }
+
+    public void InsertarAlFinal(String info){
+    	Nodo nuevo = new Nodo(info, fin);
+    	if (inicio == null){
+    		fin = nuevo;
+    		inicio = fin;
+    	}
+    	else
+    	{
+    		fin.setSiguiente(nuevo);
+    		fin = nuevo;
+    	}
+	}
 } 
