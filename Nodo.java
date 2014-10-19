2bbc5cca2aa89459fab3b07941bc46006970006b
Rodolfo Castillo <rovidrio@comunidad.unam.mx>
2014-10-19 16:33:38 -0500

    Ejemplo Listas Completado
    Todos los métodos del ejemplo funcionando

--- Nodo.java
+++ Nodo.java
@@ -13,7 +13,7 @@ public String getInfo(){    // son los get/setters de la informacion a la cual a
 	return info;            // este nodo
 }
 
-public void setInfo(){
+public void setInfo(String info){
 	this.info = info;
 }
 
@@ -21,7 +21,7 @@ public Nodo getSiguiente(){     //son los get/setters que apuntan al nodo siguie
 	return siguiente;           //enlistar
 }
 
-public void setSiguiente(){
+public void setSiguiente(Nodo siguiente){
 	this.siguiente = siguiente;
 }
 }
--- Principal.java
+++ Principal.java
@@ -3,6 +3,9 @@ public class Principal{
 		lista l = new lista();
 		l.InsertarAlInicio("a");
 		l.InsertarAlFinal("b");
+		l.InsertarAlFinal("c");
+		l.InsertarAlFinal("d");
+		l.InsertarAlFinal("e");
 		l.Listar();
 	}
 }
